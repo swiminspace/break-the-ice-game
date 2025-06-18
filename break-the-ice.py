fed29535f87ba1a080a93ef698437ea8e9049d93
porehaus <vorhaus2214@gmail.com>
2025-06-18 11:32:36 -0400

    added basic game structure

---
+++ break-the-ice.py
@@ -0,0 +1,34 @@
+
+from game2dboard import Board
+from tkinter import messagebox
+import random
+
+# Global consts
+FIELD_WIDTH = 7
+FIELD_HEIGHT = 7
+BLOCK_SIZE = 100
+MARGIN_SIZE = 15
+MSG = "ESC: Close    F2: Restart"
+
+    
+def newgame():
+    for i in range(FIELD_WIDTH):
+        for j in range(FIELD_HEIGHT):
+            if i == 0 and j == 0:
+                game[i][j] = "penguin.png"
+            else:
+                game[i][j] = "iceberg.png"
+    game.shuffle()
+    game.print(MSG)
+
+game = Board(FIELD_HEIGHT, FIELD_WIDTH)
+game.cell_size = BLOCK_SIZE
+game.title = "Break the Ice Game"
+game.margin = MARGIN_SIZE
+game.grid_color = "MediumTurquoise"
+game.margin_color = "MediumTurquoise"
+game.cell_color = "LightCyan"
+#game.on_key_press = kb_click
+#game.on_mouse_click = mouse_click
+#game.on_start = newgame
+game.show()
